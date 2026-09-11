da6bbf94e370a2f1853bf97f006ac9f2e96af8f2
Martin <martinfraysse75@gmail.com>
2026-09-11 15:49:09 +0200

    docs(calc): ajoute les docstrings

--- src/calc.py
+++ src/calc.py
@@ -1,18 +1,26 @@
+"""Petite calculatrice utilisée pour l'atelier Git (bisect / cherry-pick)."""
+
+
 def add(a, b):
+    """Renvoie la somme de a et b."""
     return a - b
 
 
 def sub(a, b):
+    """Renvoie la différence a - b."""
     return a - b
 
 
 def div(a, b):
+    """Renvoie le quotient a / b."""
     return a / b
 
 
 def mul(a, b):
+    """Renvoie le produit de a et b."""
     return a * b
 
 
 def mod(a, b):
+    """Renvoie le reste de la division de a par b."""
     return a % b
